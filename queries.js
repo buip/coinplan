244e28869d269897fb954866106be7209b7edf4a
buip <prestonbui97@gmail.com>
2018-02-02 20:39:21 -0600

    Initial commit

---
+++ queries.js
@@ -0,0 +1,20 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/puppies';
+var db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+  getAllPuppies: getAllPuppies,
+  getSinglePuppy: getSinglePuppy,
+  createPuppy: createPuppy,
+  updatePuppy: updatePuppy,
+  removePuppy: removePuppy
+};

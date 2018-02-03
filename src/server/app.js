2667f9477be42f31ed6352b177302feb4ffc0960
buip <prestonbui97@gmail.com>
2018-02-02 23:18:25 -0600

    setup folder structure

---
+++ src/server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../queries');
+
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
+
+module.exports = router;

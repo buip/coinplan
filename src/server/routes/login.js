4444a320e4a9469173890b4ce66937b419dbc0ad
buip <prestonbui97@gmail.com>
2018-02-06 13:19:32 -0600

    Create login and signup routes

---
+++ src/server/routes/login.js
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const secretToken = require('../config').secretToken;
+const validate = require('jsonschema').validate;
+
+const router = express.Router();
+
+/**
+ * Authenticate users and return a JSON Web Token with their user ID.
+ * @param {String} email
+ * @param {String} Password
+ */
+router.post('/', async (req, res) => {
+    const validParams = {
+        type: 'object',
+        maxProperties: 2,
+        required: ['email', 'password'],
+        properties: {
+            email: {
+                type: 'string',
+                minLength: 3,
+                maxLength: 254
+            },
+            password: {
+                type: 'string',
+                minLength: 3
+            }
+        }
+    };
+
+    if (!validate(req.body, validParams).valid) {
+        res.sendStatus(400);
+    } else {
+        try {
+            const user = await User.getByEmail(req.body.email);
+            const isValid = await bcrypt.compare(req.body.password, user.passwordHash);
+            if (isValid) {
+                const token = jwt.sign({ data: user.id }, secretToken, { expiresIn: 86400 });
+                res.json({ token: token });
+            } else {
+                throw new Error('Unauthorized password');
+            }
+        } catch (err) {
+            if (err.message === 'Unauthorized password' || err.message === 'No data returned from the query.') {
+                res.status(401).send({ text: 'Not authorized' });
+            } else {
+                res.status(500).send({ text: 'Internal Server Error' });
+            }
+        }
+    }
+});
+
+module.exports = router;
